246a3c7a01b59907eb93c97548cb68851a5a230b
Catherine Lee <CLee@MacBook-Pro-2.local>
2021-07-19 10:14:34 -0700

    update python code

---
+++ BERT/containers.py
@@ -0,0 +1,33 @@
+import torch
+from dataclasses import dataclass
+from typing import List, Any
+
+IntList = List[int]  # A list of token_ids
+IntListList = List[IntList]  # A List of List of token_ids, e.g. a Batch
+
+@dataclass
+class TrainingExample:
+    input_ids: IntList
+    attention_mask: IntList
+    labels: IntList
+
+
+class TraingingBatch:
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def __init__(self, examples: List[TrainingExample]):
+        self.input_ids: torch.Tensor
+        self.attention_mask: torch.Tensor
+        self.labels: torch.Tensor
+        input_ids: IntListList = []
+        masks: IntListList = []
+        labels: IntListList = []
+        for ex in examples:
+            input_ids.append(ex.input_ids)
+            masks.append(ex.attention_mask)
+            labels.append(ex.labels)
+        self.input_ids = torch.LongTensor(input_ids)
+        self.attention_mask = torch.LongTensor(masks)
+        self.labels = torch.LongTensor(labels)
+        
---
+++ BERT/prepareData.py
@@ -0,0 +1,46 @@
+import re
+
+def get_annotations(text, pattern): 
+    """Helper function for prepare_data
+
+    Args:
+        text (str): Input string
+        pattern (regex expression): pattern we are looking for from the input string
+
+    Returns:
+        list: A list of dictionaries recording matches we found. E.g. [{'start': int, 'end': int, 'label': str}, ]
+    """
+    annotations = []
+    # find all strings matching the input pattern.
+    for match in re.finditer(pattern, text): 
+        label_dic = dict()
+        label_dic['start'] = match.start()
+        label_dic['end'] = match.end()
+        label_dic['label'] = 'CLEntity' # Entity starting with a capital letter.
+        annotations.append(label_dic)
+    return annotations    
+
+def prepare_data(FileName, pattern): 
+    """Find all strings matching the input pattern within the text file.
+
+    Args:
+        FileName (str): Path to a text file
+        pattern (regex expression): pattern we are looking for
+
+    Returns:
+        list : A list of dictionaries. E.g. {'content': str, 'annotations': list(dict)}
+    """
+    re.compile(pattern)
+    dataset = []
+
+    book = open(FileName)
+  
+    for line in book:
+        line = line.strip()
+
+        line_data = dict()
+        line_data['content'] = line
+        line_data['annotations'] = get_annotations(line, pattern)
+        dataset.append(line_data)
+
+    return dataset
